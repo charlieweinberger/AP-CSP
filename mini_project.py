4f5ba65c3e6e20a031931aa6729c9ae3e8867be6
Charlie Weinberger <364charliebrown@gmail.com>
2022-08-17 21:34:31 +0000

    updated mini project

--- mini_project.py
+++ mini_project.py
@@ -1,17 +1,11 @@
-age = input("When is your birthday? (MM/DD/YYYY) ")
-age = age.split("/")
-
-birth_month = int(age[0][1]) if age[0][0] == '0' else int(age[0])
-birth_day = int(age[1][1]) if age[1][0] == '0' else int(age[1])
-birth_year = int(age[2])
-
-earth_years = 2022 - birth_year + ((8 - birth_month) * 10 / 12) / 10
-earth_days = birth_day + 365.26 * earth_years
+DAYS_IN_YEAR = 365.26
+MONTHS_IN_YEAR = 12
+CURRENT_YEAR = 2022
+CURRENT_MONTH = 8
 
 planet_data = {
     'Mercury': [58.6, 0.241],
     'Venus': [243, 0.615],
-    'Earth': [0.99, 1],
     'Mars': [1.03, 1.88],
     'Jupiter': [0.41, 11.86],
     'Saturn': [0.45, 29.46],
@@ -20,7 +14,17 @@ planet_data = {
     'Pluto': [6.39, 248.59]
 }
 
+age = input("When is your birthday? (MM/DD/YYYY) ")
+age = age.split("/")
+
+birth_month = int(age[0][1]) if age[0][0] == '0' else int(age[0])
+birth_day   = int(age[1][1]) if age[1][0] == '0' else int(age[1])
+birth_year  = int(age[2])
+
+earth_years = CURRENT_YEAR - birth_year + (CURRENT_MONTH - birth_month) / MONTHS_IN_YEAR
+earth_days = birth_day + DAYS_IN_YEAR * earth_years
+
 for planet, data in planet_data.items():
     days  = round(earth_days  / data[0], 2)
     years = round(earth_years / data[1], 2)
-    print(f"Your age on {planet} is {days} days, or {years} years.")
+    print(f"Your age on {planet} is approximately {days} days, or {years} years.")
