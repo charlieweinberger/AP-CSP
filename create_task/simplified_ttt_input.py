53417ef412578a62e29f2f040bbf82652db40fc5
Charlie Weinberger <364charliebrown@gmail.com>
2023-04-26 18:54:55 +0000

    really finished create task

--- create_task/simplified_ttt_input.py
+++ create_task/simplified_ttt_input.py
@@ -50,5 +50,8 @@ while True:
         winner = check_for_winner(board)
         
         if winner != None:
+            print(board[0] + "|" + board[1] + "|" + board[2])
+            print(board[3] + "|" + board[4] + "|" + board[5])
+            print(board[6] + "|" + board[7] + "|" + board[8])
             print(winner)
             exit()
